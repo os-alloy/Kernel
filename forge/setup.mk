b0052846aa30fe092f235a89f9570660076065a9
Yuval Dolev <yuvaldolev2@gmail.com>
2021-01-01 02:13:30 +0200

    Removed forge makefiles

--- forge/setup.mk
+++
@@ -1,9 +0,0 @@
-# Setup the default target.
-include forge/setup_default_target.mk
-
-# Perform recursive build if it's necessary.
-include forge/setup_recursive_build.mk
-
-# From this point we're in the main build process.
-# Setup the build.
-include forge/setup_build.mk
